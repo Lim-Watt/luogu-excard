4e5752a580b9ce41b300a9b6087dbde26e6546d2
Lim-Watt <150017579+Lim-Watt@users.noreply.github.com>
2024-04-11 07:16:26 +0800

    Update common.js

--- src/common.js
+++ src/common.js
@@ -47,10 +47,12 @@ const reNumSVG = (stats) => {
     if(hideInfo) {
         return renderError("用户开启了“完全隐私保护”，获取数据失败", options={width:360});
     }
-    return renderError(String(passed[7] - passed[6] + 1), options={width:360});
-    // return new ANum({
-    //     number: passed[7] - passed[6] + 1,
-    // }).render();
+    // return renderError(String(passed[7] - passed[6] + 1), options={width:360});
+    return new ANum({
+        number: passed[7] - passed[6] + 1,
+        cf: "#e74c3c",
+        cb: "#fffefe",
+    }).render();
 }
 
 class Card {
