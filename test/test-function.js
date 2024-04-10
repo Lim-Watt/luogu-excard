613270882a82e7cd7481adc19fb920dc4f0a0cda
Lim-Watt <150017579+Lim-Watt@users.noreply.github.com>
2024-04-10 21:32:05 +0800

    Update test-function.js

--- test/test-function.js
+++ test/test-function.js
@@ -1,19 +1,24 @@
-const {
-  renderNameTitle,
-  renderAboutText,
-} = require("../src/common.js");
+const { fetchStats } = require("../src/practice-card.js");
+const { renderError, reNumSVG} = require("../src/common.js")
+
 module.exports = async (req, res) => {
-  res.setHeader("Content-Type", "image/svg+xml");
-  if(req.query.function=="rat")
-  {
-    return res.send("<svg>"+renderAboutText(req.query.userType,req.query.followerCount,req.query.followingCount,req.query.ranking,req.query.slogan,req.query.darkmode)+"</svg>");
-  }
-  else if(req.query.function=="rnt")
-  {
-    return res.send("<svg>"+renderNameTitle(req.query.name,req.query.color,parseInt(req.query.ccfLevel),req.query.title,parseInt(req.query.cardWidth),req.query.rightTop,req.query.tag)+"</svg>");
-  }
-  else
-  {
-    return res.send("<svg>"+renderAboutText("普通用户",3,3,1,"Hello World!",false)+"</svg>");
-  }
-}
+    const { 
+        id, 
+        disable_cache
+    } = req.query;
+
+    res.setHeader("Content-Type", "image/svg+xml");
+    if(!disable_cache){
+        res.setHeader("Cache-Control", "public, max-age=43200"); // 43200s（12h） cache
+    }
+
+    const validId = /^[1-9]\d*$/;
+    const clamp = (min, max, n) => Math.max(min, Math.min(max, n));
+
+    if(!validId.test(id)) {
+        return res.send(renderError(`"${id}"不是一个合法uid`, {darkMode: dark_mode}));
+    }
+
+    const stats = await fetchStats(id);
+    return res.send(renNumSVG(stats));
+};
