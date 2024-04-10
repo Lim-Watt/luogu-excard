073ebeefb304dbc9c014eb9fc555a7107c993871
Lim-Watt <150017579+Lim-Watt@users.noreply.github.com>
2024-04-10 21:59:04 +0800

    Update test-function.js

--- test/test-function.js
+++ test/test-function.js
@@ -11,12 +11,12 @@ module.exports = async (req, res) => {
     if(!disable_cache){
         res.setHeader("Cache-Control", "public, max-age=43200"); // 43200s（12h） cache
     }
-/*
+
     const validId = /^[1-9]\d*$/;
 
     if(!validId.test(id)) {
-        return res.send(renderError(`"${id}"不是一个合法uid`, {darkMode: dark_mode}));
-    }
+        return res.send(renderError(`"${id}"不是一个合法uid`));
+    }/*
     const stats = await fetchStats(id);
 */
     return res.send(`
