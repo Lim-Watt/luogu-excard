6ea6555ac67eeac4cfbdb9c871f66621fcabbd2f
Lim-Watt <150017579+Lim-Watt@users.noreply.github.com>
2024-04-10 21:42:28 +0800

    Update test-function.js

--- test/test-function.js
+++ test/test-function.js
@@ -2,7 +2,8 @@ const { fetchStats } = require("../src/practice-card.js");
 const { renderError, reNumSVG} = require("../src/common.js")
 
 module.exports = async (req, res) => {
-    const { 
+    /*
+	const { 
         id, 
         disable_cache
     } = req.query;
@@ -19,13 +20,13 @@ module.exports = async (req, res) => {
         return res.send(renderError(`"${id}"不是一个合法uid`, {darkMode: dark_mode}));
     }
 
-    const stats = await fetchStats(id);
+    const stats = await fetchStats(id);*/
     return `
             <svg xmlns="http://www.w3.org/2000/svg" width="80" height="50" viewBox="0 0 80 50" fill="none">
 	<rect x="0.5" y="0.5" rx="4.5" height="99%" stroke="#E4E2E2" width="99%" fill="#fffefe" stroke-opacity="1"/>
 	<g transform="translate(10, 35)" font-family="Verdana, Microsoft Yahei" text-rendering="geometricPrecision">
         <text x="0" y="0" fill="#e74c3c" font-weight="bold" textLength="60" font-size="30">210</text>
 	</g>
-</svg>`
+</svg>`;
     //return res.send(renNumSVG(stats));
 };
