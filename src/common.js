d8477541e321e515ce806db560f78293a34411c9
Lim-Watt <150017579+Lim-Watt@users.noreply.github.com>
2024-04-11 21:01:01 +0800

    Update common.js

--- src/common.js
+++ src/common.js
@@ -24,9 +24,9 @@ class ANum {
     render() {
 	    
         return `
-            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="50" viewBox="0 0 80 50" fill="none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="40" viewBox="0 0 80 40" fill="none">
             	<rect x="0.5" y="0.5" rx="4.5" height="99%" stroke="#E4E2E2" width="99%" fill="${this.cb}" stroke-opacity="1"/>
-            	<g transform="translate(10, 35)" font-family="Verdana, Microsoft Yahei" text-rendering="geometricPrecision">
+            	<g transform="translate(10, 30)" font-family="Verdana, Microsoft Yahei" text-rendering="geometricPrecision">
                     <text x="0" y="0" fill="${this.cf}" font-weight="bold" textLength="60" font-size="30">${this.number}</text>
             	</g>
             </svg>`;
