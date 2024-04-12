d8eb2f9ae589213611e93081b944f0793230c8a2
Lim-Watt <150017579+Lim-Watt@users.noreply.github.com>
2024-04-12 20:16:45 +0800

    Update common.js

--- src/common.js
+++ src/common.js
@@ -57,7 +57,7 @@ class ANum {
 			<style>
 				@font-face{
 					font-family: 'Code' ;
-					src: url('/ttf/Code.ttf');
+					src: url('/api/ttf/Code.ttf');
 				}
 				text {
 					font-family: Code, Verdana, Microsoft Yahei;
